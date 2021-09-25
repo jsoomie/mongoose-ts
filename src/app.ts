5157fc5d6f8513d40a65a47cf6ee0fc2a1d53899
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-24 20:47:03 -0700

    feat: create starter files connected to mongodb

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import config from "config";
+import log from "./logger";
+import { connection } from "./db/connect";
+
+const PORT = config.get("port") as number;
+const HOST = config.get("host") as string;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, HOST, () => {
+  log.info(`Server listening at http://${HOST}:${PORT}`);
+  connection();
+});
---
+++ src/db/connect.ts
@@ -0,0 +1,21 @@
+import { ConnectOptions, connect } from "mongoose";
+import config from "config";
+import log from "../logger";
+
+export const connection = async () => {
+  try {
+    const dbUri = config.get("dbUri") as string;
+    const db = await connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+
+    log.info("Database connected");
+    return db;
+  } catch (error) {
+    log.error("db error", error);
+    process.exit(1);
+  }
+};
+
+export default connection;
