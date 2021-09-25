a04bf75f07c96b1aee519ae0e8a96aeddb7fbb0d
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-24 21:32:01 -0700

    feat: add user models gave passwrod salt and hash.

--- routes.ts
+++ routes.ts
@@ -2,4 +2,16 @@ import { Express, Request, Response } from "express";
 
 export const routes = (app: Express) => {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  // Register
+  // POST /api/user
+
+  // Login
+  // POST /api/session
+
+  // Get User's sessions
+  // GET /api/sessions
+
+  // Logout
+  // DELETE /api/sessions
 };
