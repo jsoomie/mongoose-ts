1b1aa5b2ca61cbd18e67f710350df77032f1fe4a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 09:25:57 -0700

    feat: add create user controller imported omit from lodash to omit users password.

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,11 @@
 import { Response, Request } from "express";
 import { createUser } from "../service/";
+import { omit } from "lodash";
 
 export const createUserHandler = async (req: Request, res: Response) => {
   try {
-    //Create User Here
+    const user = await createUser(req.body);
+    return res.send(omit(user, "password"));
   } catch (error) {
     const { message } = error as Error;
     return { message: `User Controller: ${message}` };
