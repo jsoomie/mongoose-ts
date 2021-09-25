2db5fd3769ce4981babdbd5c9871e0b4b241a1d9
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 12:09:40 -0700

    feat: Create validatePassword in user.service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,11 +1,37 @@
 import { DocumentDefinition } from "mongoose";
 import { User, UserDocument } from "model";
+import { omit } from "lodash";
 
 export const createUser = async (input: DocumentDefinition<UserDocument>) => {
   try {
     return await User.create(input);
   } catch (error: unknown) {
     const { message } = error as Error;
-    return { message: `User Services: ${message}` };
+    return { message: `createUser: ${message}` };
+  }
+};
+
+export const validatePassword = async ({
+  email,
+  password,
+}: {
+  email: UserDocument["email"];
+  password: string;
+}) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return false;
+    }
+
+    const isValid = await user.comparePassword(password);
+    if (!isValid) {
+      return false;
+    }
+
+    return omit(user, "password");
+  } catch (error) {
+    const { message } = error as Error;
+    return { message: `validatePassword: ${message}` };
   }
 };
