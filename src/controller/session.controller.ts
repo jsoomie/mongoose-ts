21e33a971195cc9ec9a3c437ea0298bab04b422e
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 12:17:20 -0700

    fix(controller): Remove half created session creation for user session creation has not be created yet

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,17 +1,16 @@
 import { Response, Request } from "express";
 import { log } from "logger";
-import { validatePassword } from "service"
+import { validatePassword } from "service";
 
 export const createUserSessionHandler = async (req: Request, res: Response) => {
   try {
     // Validates password
     const user = await validatePassword(req.body);
-    if(!user) {
+    if (!user) {
       return res.status(401).send("Invalid username or password");
     }
 
     // Create a session for user
-    const session = await 
   } catch (error) {
     const { message } = error as Error;
     log.error(error as Error);
