87604940159a76c218baf308cda874f06018dbfb
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 10:16:50 -0700

    docs: Add comment for healthcheck route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Express, Request, Response } from "express";
 
 export const routes = (app: Express) => {
+  // Dev Check
+  // GET /healthcheck
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   // Register
