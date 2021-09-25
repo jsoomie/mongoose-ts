eeaffd5eea7ff86b7682f1c23703f7a3a8641108
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 10:30:35 -0700

    feat: Create middleware validation validates requests otherwise return error.

--- src/middleware/validateRequests.ts
+++ src/middleware/validateRequests.ts
@@ -1,1 +1,22 @@
-// Validate Requests here
+import { AnySchema } from "yup";
+import { Request, Response, NextFunction } from "express";
+import { log } from "../logger";
+
+export const validate =
+  (schema: AnySchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validate({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      return next();
+    } catch (err) {
+      const { message } = err as Error;
+      log.error(message);
+      res.send(message).status(400);
+      return { message: `Validate Request: ${message}` };
+    }
+  };
