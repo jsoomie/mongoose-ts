3ce44a0ccbdcc8a7112d7ff88109da01be5b29d1
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-25 20:53:24 -0700

    feat: Create access token for user Create jwt.utils files to handle singing token privatekey stored in config

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,5 +1,8 @@
-import { Session, SessionDocument } from "../model";
+import { Session, SessionDocument, UserDocument } from "../model";
 import { log } from "../logger";
+import { LeanDocument } from "mongoose";
+import config from "config";
+import { sign } from "../utils/jwt.utils";
 
 export const createSession = async (userId: string, userAgent: string) => {
   try {
@@ -11,3 +14,23 @@ export const createSession = async (userId: string, userAgent: string) => {
     return { message: `createSession: ${message}` };
   }
 };
+
+export function createAccessToken({
+  user,
+  session,
+}: {
+  user:
+    | Omit<UserDocument, "password">
+    | LeanDocument<Omit<UserDocument, "password">>;
+  session:
+    | Omit<SessionDocument, "password">
+    | LeanDocument<Omit<SessionDocument, "password">>;
+}) {
+  // Build and return the new access token
+  const accessToken = sign(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTtl") }
+  );
+
+  return accessToken;
+}
